5514bca716ea1a01817cba6d9371b30db8644c63
jake <palatable7@naver.com>
2022-03-01 19:07:54 +0900

    Add API.swift

--- ExApollo/ViewController.swift
+++ ExApollo/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import Apollo
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    // singletown으로 사용하지 않으면 sessionInvalidated에러 발생
+    let apollo = ApolloClient(url: URL(string: "https://apollo-fullstack-tutorial.herokuapp.com/graphql")!)
+    apollo.fetch(query: MyQueryQuery()) { result in
+      switch result {
+      case .success(let data):
+        print(data.data?.launches.cursor)
+      case .failure(let error):
+        print(error) // sessionInvalidated
+      }
+    }
+    
+    Network.shared.apollo.fetch(query: MyQueryQuery()) { result in
+      switch result {
+      case .success(let data):
+        print(data.data?.launches.cursor)
+      case .failure(let error):
+        print(error)
+      }
+    }
   }
-
-
 }
+ 
+class Network {
+  static let shared = Network()
 
+  private(set) lazy var apollo = ApolloClient(url: URL(string: "https://apollo-fullstack-tutorial.herokuapp.com/graphql")!)
+}
